e0ea54da673b50ffcbc8c580a37962675b7d32b3
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-10-01 15:34:10 +0000

    premier projet

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Admin from './components/admin/Admin';
+import Utilisateur from './pages/utilisateurs/Utilisateur';
+function App() {
+  return (
+    <div className="App">
+      <h1>Material-UI Buttons</h1>
+      <h1>Resouder un bug</h1>
+      <Admin />
+      <Utilisateur />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/utilisateurs/Utilisateur.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Utilisateur = () => {
+  return (
+    <div>
+      <h1>Utilisateur gestion livre</h1>
+    </div>
+  )
+}
+
+export default Utilisateur
