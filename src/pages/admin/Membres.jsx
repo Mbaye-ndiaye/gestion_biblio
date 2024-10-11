ae7189fa29356764552ff74ee1bcdf1e935672de
Ladji Timera <ladjitimera52@gmail.com>
2024-10-11 17:43:30 +0000

    pusher des modification

--- src/pages/admin/Membres.jsx
+++ src/pages/admin/Membres.jsx
@@ -1,7 +1,7 @@
-import { Box, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import React from "react";
 import Sidebar from "../../components/sidebar/Sidebar";
-
+import Tables from "../../components/table/Table";
 
 export default function Membres() {
   return (
@@ -9,7 +9,24 @@ export default function Membres() {
       <Box sx={{ display: "flex" }}>
         <Sidebar />
         <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
-          <Typography variant="h4">Membres</Typography>
+          <Tables
+            headerValues={["Nom", "Age", "Pays", "Role", "Action"]}
+            rows={[
+              {
+                name: "santos",
+                age: 25,
+                country: "sénégal",
+                role: "Developer",
+                action: "detail",
+              },
+              {
+                name: " Smith",
+                age: 30,
+                country: "mali",
+                role: "Designer",
+              },
+            ]}
+          />
         </Box>
       </Box>
     </>
