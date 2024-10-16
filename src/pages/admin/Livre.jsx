50c0c9eeaaf770b42d30ec6277e2744399b33a14
Ladji Timera <ladjitimera52@gmail.com>
2024-10-16 16:33:45 +0000

    pusher des modification

--- src/pages/admin/Livre.jsx
+++ src/pages/admin/Livre.jsx
@@ -1,14 +1,56 @@
-import { Box } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
+import { Grid, Box, Typography, Modal, Button } from "@mui/material";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Tables from "../../components/table/Table";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import TextField from "@mui/material/TextField";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  bgcolor: "white",
+  color: "black",
+  border: "none",
+  borderRadius: "10px",
+  boxShadow: 24,
+  p: 4,
+};
 
 export default function Livre() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const handleModalOpen = () => {
+    setModalOpen(true);
+  };
+
+  const handleModalClose = () => {
+    setModalOpen(false);
+  };
+
   return (
     <>
       <Box sx={{ display: "flex" }}>
         <Sidebar />
         <Box component="main" sx={{ flexGrow: 1, p: 3, marginTop: "55px" }}>
+          <Typography
+            sx={{
+              float: "right",
+              width: "5%",
+              textAlign: "center",
+              color: "white",
+              paddingTop: "7px",
+              backgroundColor: "gray",
+              borderRadius: "10px",
+              marginBottom: 2,
+              cursor: "pointer",
+            }}
+            onClick={handleModalOpen}
+          >
+            <AddCircleIcon sx={{ fontSize: "35px" }} />
+          </Typography>
           <Tables
             headerValues={["Name", "Age", "Country", "Role"]}
             rows={[
@@ -23,6 +65,92 @@ export default function Livre() {
           />
         </Box>
       </Box>
+      <Modal open={modalOpen} onClose={handleModalClose}>
+        <Box sx={modalStyle}>
+          {/* Contenu du modal ici */}
+          <Box sx={{ p: 2 }}>
+            <Box component="form" noValidate sx={{ mt: 3, maxWidth: 945 }}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    autoComplete="given-name"
+                    name="firstName"
+                    required
+                    fullWidth
+                    id="firstName"
+                    label="First Name"
+                    autoFocus
+                    defaultValue="Ladji"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="lastName"
+                    label="Last Name"
+                    name="lastName"
+                    autoComplete="family-name"
+                    defaultValue="Timera"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="email"
+                    label="Email"
+                    name="email"
+                    autoComplete="email"
+                    defaultValue="tims7788@gmail.com"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="address"
+                    label="Address"
+                    name="address"
+                    autoComplete="address"
+                    defaultValue="mbao"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    required
+                    fullWidth
+                    id="contactNumber"
+                    label="Contact Number"
+                    name="contactNumber"
+                    autoComplete="contact-number"
+                    defaultValue="787907654"
+                  />
+                </Grid>
+              </Grid>
+
+              <Typography
+                sx={{
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "40%",
+                  textAlign: "center",
+                  color: "white",
+                  paddingY: "10px",
+                  backgroundColor: "gray",
+                  borderRadius: "10px",
+                  cursor: "pointer",
+                  marginTop: 3,
+                }}
+                onClick={handleModalOpen}
+              >
+                Enregistrer
+              </Typography>
+            </Box>
+          </Box>
+        </Box>
+      </Modal>
     </>
   );
 }
